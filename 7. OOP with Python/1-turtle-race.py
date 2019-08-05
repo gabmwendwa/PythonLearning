29d3965d11012cc19b4c0ef606538fd648d97d6e
Gabriel Mutisya <gabrielmwendwa@gmail.com>
2019-08-05 12:51:40 +0300

    Turtle Race with Python complete

--- 7. OOP with Python/1-turtle-race.py
+++ 7. OOP with Python/1-turtle-race.py
@@ -1,7 +1,54 @@
-import turtle as Turtle
+import os
+from turtle import Turtle
+from random import randint
+# Ninja Turtles
 
-Danotello = Turtle.color('purple')
-Danotello = Turtle.shape('turtle')
-Danotello = Turtle.goto(160, 100)
+# Danotello 
+Danotello = Turtle()
 
+Danotello.color('purple')
+Danotello.shape('turtle')
+
+Danotello.penup()
+Danotello.goto(-160, 100)
+Danotello.pendown()
+
+# Raphael
+Raphael = Turtle()
+
+Raphael.color('red')
+Raphael.shape('turtle')
+
+Raphael.penup()
+Raphael.goto(-160, 70)
+Raphael.pendown()
+
+
+# Leonardo
+Leonardo = Turtle()
+
+Leonardo.color('blue')
+Leonardo.shape('turtle')
+
+Leonardo.penup()
+Leonardo.goto(-160, 40)
+Leonardo.pendown()
+
+# Michaelangelo
+Michaelangelo = Turtle()
+
+Michaelangelo.color('orange')
+Michaelangelo.shape('turtle')
+
+Michaelangelo.penup()
+Michaelangelo.goto(-160, 10)
+Michaelangelo.pendown()
+
+for movement in range(100):
+	Danotello.forward(randint(1,5))
+	Raphael.forward(randint(1,5))
+	Leonardo.forward(randint(1,5))
+	Michaelangelo.forward(randint(1,5))
+ 
+os.system("pause")
 
