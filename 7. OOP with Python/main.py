590c7a204da11c3166f685d6f529466cb6cfcf51
Gabriel Mutisya <gabrielmwendwa@gmail.com>
2019-08-13 12:31:45 +0300

    Room game with Python

--- 7. OOP with Python/main.py
+++ 7. OOP with Python/main.py
@@ -5,6 +5,37 @@ kitchen = Room()
 kitchen.set_name("Kitchen")
 kitchen.set_description("A dank and dirty room buzzing with flies.")
 
+"""
 print(kitchen.get_name())
 print(kitchen.get_description())
 kitchen.describe()
+"""
+dining_hall = Room()
+
+dining_hall.set_name("Dining Hall")
+dining_hall.set_description("A large room with tables and chairs.")
+
+ballroom = Room()
+
+ballroom.set_name("Ballroom")
+ballroom.set_description("A vast room with shiny artifacts.")
+
+
+kitchen.link_room(dining_hall, "south")
+dining_hall.link_room(kitchen, "north")
+dining_hall.link_room(ballroom, "west")
+ballroom.link_room(dining_hall, "east")
+
+"""
+kitchen.get_details()
+dining_hall.get_details()
+ballroom.get_details()
+"""
+current_room = kitchen
+
+while True:
+	print( "\n" )
+	current_room.get_details()
+	command = input("> ")
+	current_room = current_room.move(command)
+	
--- 7. OOP with Python/room.py
+++ 7. OOP with Python/room.py
@@ -2,6 +2,7 @@ class Room():
 	def __init__(self):
 		self.name = None
 		self.description = None
+		self.linked_rooms = {}
 		
 	def set_name(self, room_name):
 		self.name = room_name
@@ -17,3 +18,23 @@ class Room():
 		
 	def describe(self): 
 		print(self.description)
+	
+	def link_room(self, room_to_link, direction):
+		self.linked_rooms[direction] = room_to_link
+		# print( self.name + " linked rooms :" + repr(self.linked_rooms) )
+	
+	def get_details(self):
+		for direction in self.linked_rooms:
+			room = self.linked_rooms[direction]
+			print( "The " + room.get_name())
+			print( "--------------------" )
+			print( room.get_description() )
+			print( "The " + room.get_name() + " is " + direction )
+			print( "\n" )
+	
+	def move(self, direction):
+		if direction in self.linked_rooms:
+			return self.linked_rooms[direction]
+		else:
+			print( "You cannot go that way" )
+			return self
