c187aa2cdd11ab4d287e8a548597b619aed65742
Gabriel Mutisya <gabrielmwendwa@gmail.com>
2019-09-02 10:06:58 +0300

    Adding new attributes and methods

--- 7. OOP with Python/character.py
+++ 7. OOP with Python/character.py
@@ -29,4 +29,20 @@ class Character():
 
 class Enemy(Character):
 	def __init__(self, char_name, char_description):
+		
 		super().__init__(char_name, char_description)
+		self.weakness = None
+	
+	def fight(self, combat_item):
+		if combat_item == self.weakness:
+			print("You fend " + self.name + " off with the " + combat_item )
+			return True
+		else:
+			print(self.name + " crushes you, puny adventurer")
+			return False
+			
+	def set_weakness(self, item_weakness):
+		self.weakness = item_weakness
+		
+	def get_weakness(self):
+		return self.weakness
--- 7. OOP with Python/character_test.py
+++ 7. OOP with Python/character_test.py
@@ -10,24 +10,10 @@ dave.set_conversation("I want your brains!")
 # Trigger a conversation with Dave
 dave.talk()
 
-# Trigger fight with Dave
-dave.fight("Knife")
+dave.set_weakness("water")
 
-jack = Character("Jack", "The Hero")
+print("What will you fight with?")
 
-jack.describe()
+fight_with = input()
 
-# Add some conversation for Jack when he is talked to
-jack.set_conversation("What's up, dude!")
-
-# Trigger a conversation with Jack
-jack.talk()
-
-# Add some conversation for Jack when he is talked to
-jack.set_conversation("No brains for you today!")
-
-# Trigger a conversation with Jack
-jack.talk()
-
-# Trigger fight with Jack
-jack.fight("Shotgun")
+dave.fight(fight_with)
