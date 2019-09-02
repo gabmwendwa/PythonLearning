ec122ef4b88fd0a164710d0387b99bb27bf2ba79
Gabriel Mutisya <gabrielmwendwa@gmail.com>
2019-09-02 13:48:37 +0300

    More interactions and challenges

--- 7. OOP with Python/character.py
+++ 7. OOP with Python/character.py
@@ -32,6 +32,7 @@ class Enemy(Character):
 		
 		super().__init__(char_name, char_description)
 		self.weakness = None
+		self.item_to_steal = None
 	
 	def fight(self, combat_item):
 		if combat_item == self.weakness:
@@ -46,3 +47,33 @@ class Enemy(Character):
 		
 	def get_weakness(self):
 		return self.weakness
+				
+	def set_item_to_steal(self, item_steal):
+		self.item_to_steal = item_steal
+		
+	def get_item_to_steal(self):
+		return self.item_to_steal
+
+	def steal(self, item_steal):
+		print("You steal from "+ self.name)
+		# How will you decide what this character has to steal?
+		if item_steal == self.item_to_steal:
+			print("You have stolen "+ item_steal + " from " + self.name)
+			return True
+		else:
+			print(self.name + " does not have "+ item_steal)
+			return False
+
+class Friend(Character):
+	def __init__(self, char_name, char_description):
+		
+		super().__init__(char_name, char_description)
+		self.feeling = None
+	
+	def hug(self, char_feel):
+		if char_feel == "yes":
+			print(self.name + " hugs you back!")
+			return True
+		else:
+			print(self.name + " is now sad as well!")
+			return False
--- 7. OOP with Python/main.py
+++ 7. OOP with Python/main.py
@@ -1,5 +1,5 @@
 from room import Room
-from character import Enemy
+from character import Enemy, Friend
 
 kitchen = Room("Kitchen")
 # kitchen.set_name("Kitchen")
@@ -26,12 +26,25 @@ dave.set_conversation("I want your brains!")
 # Set Dave's weakness attribute
 dave.set_weakness("water")
 
+# Set Dave's item to steal attribute
+dave.set_item_to_steal("ring")
+
 # Add Dave to dining hall
 dining_hall.set_character(dave)
 
+# Add new friendly character
+catrina = Friend("Catrina","A friendly skeleton")
+
+catrina.set_conversation("Why hello there.")
+
+# Add Catrina to room
+ballroom.set_character(catrina)
+
 current_room = kitchen
 
-while True:
+dead = False
+
+while dead == False:
 	print( "\n" )
 	current_room.get_details()
 	
@@ -41,4 +54,61 @@ while True:
 		inhabitant.describe()
 	
 	command = input("> ")
-	current_room = current_room.move(command)
+	
+	if command in ["north", "east", "south", "west"]:
+		# Move in given direction
+		current_room = current_room.move(command)
+	elif command == "talk":
+		# Talk to the inhabitant
+		inhabitant.talk()
+		response = input("reply: ")
+		print("You said: "+response)
+	elif command == "fight":
+		# Fight with the inhabitant
+		print("What will you fight with?")
+		fight_with = input("> ")
+		if inhabitant.fight(fight_with) == True:
+			# What happens if you win?
+			print("You win!")
+			msg = "Move to the next room."
+			if current_room == kitchen:
+				print("Move to dining hall [south]")
+				command = input("> ")
+			elif current_room == dining_hall:
+				print("Move to kitchen [north] or ballroom [west]")
+				command = input("> ")
+			else:
+				print("Move to dining room [east]")
+				command = input("> ")
+			current_room = current_room.move(command)
+		else:
+			# What happens if you loose?
+			print("Better luck next time!")
+			dead = True
+	elif command == "steal":
+		print("What will you steal?")
+		item_steal = input("> ")
+		if inhabitant.steal(item_steal) == True:
+			msg = "Move to the next room."
+			if current_room == kitchen:
+				print("Move to dining hall [south]")
+				command = input("> ")
+			elif current_room == dining_hall:
+				print("Move to kitchen [north] or ballroom [west]")
+				command = input("> ")
+			else:
+				print("Move to dining room [east]")
+				command = input("> ")
+			current_room = current_room.move(command)
+		else:
+			# What happens if you loose?
+			print("Better luck next time!")
+			dead = True
+	elif command == "sad":
+		print("Do you want a hug? [yes/no]")
+		ans = input("> ")
+		if inhabitant.hug(ans) == True:
+			print("Yay!")
+		else:
+			print("Better luck next time!")
+			dead = True
